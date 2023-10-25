db345342933adef98ae6d342e39a6221b74b6ab9
Boopeshvik <boopeshvikram.r@outlook.com>
2023-10-25 21:20:41 +0100

    Adding two new files

---
+++ Variables and Types.py
@@ -0,0 +1,30 @@
+#Integer
+int = 10
+print(int)
+int_type = type(int)
+print(int_type)
+
+
+#Floting
+float = 6.8
+type_1 = type(float)
+print(type_1)
+
+#String
+my_string = "I love India"
+string_type = type(my_string)
+print(my_string,string_type)
+
+#The target of this exercise is to create a string, an integer, and a floating point number. The string should be named mystring and should contain the word "hello". The floating point number should be named myfloat and should contain the number 10.0, and the integer should be named myint and should contain the number 20.
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+print(mystring)
+print(myfloat)
+print(myint)
+
+
+
+
