7cbd7c9f4e27835aeb2892a780f96017b30f1e4e
Boopeshvik <boopeshvikram.r@outlook.com>
2023-10-29 21:19:53 +0000

    Adding Hacker rank problems

---
+++ Hacker_Rank_Arithematic.py
@@ -0,0 +1,13 @@
+a = input("Enter the valeu of a")
+a = int(a)
+b = input ("Enter the value for b")
+b = int(b)
+first_line = a+b
+print(first_line)
+second_line = a-b
+print(second_line)
+third_line = a*b
+print(third_line)
+
+# a//b -> integer division
+#a/b -> floating division
