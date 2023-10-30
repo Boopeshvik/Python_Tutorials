ac3270e3eba2753c912e885a2b3c91073fa3b0ea
Boopeshvik <boopeshvikram.r@outlook.com>
2023-10-30 10:20:39 +0000

    Adding Project 1

---
+++ Project 1_Madlibs.py
@@ -0,0 +1,14 @@
+#Madlibs #string Concatination
+
+#youtuber = "Boopeshvikram"
+#print(f"Subscribe to {youtuber}")
+
+adj = input("Enter the Adjective:")
+verb1 = input("Enter the verb1:")
+verb2 = input("Enter the verb2:")
+famous_person = input("Enter the famous person name:")
+
+madlib = f"Business analyst carrier is very {adj}! It will keep you busy and intresting at all times because \
+I love to {verb1}. Stay humble and {verb2} like you are {famous_person}!"
+
+print(f"The final Matlib is {madlib}")
